be036eefa695e658de0bda3075e7a6c02055ef89
Shybo <lb1505566540@163.com>
2021-10-10 11:09:39 +0800

    完成了主页功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,62 @@
+$(function() {
+    //调用获取基本信息
+    getUserInfo()
+
+    var layer = layui.layer
+
+    //点击按钮实现退出功能
+    $('#btnLogout').on('click',function() {
+        //提示用户是否退出
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            //1.清空本地存储的token
+            localStorage.removeItem('token')
+            //2.跳转登录页
+            location.href = '/login.html'
+
+            //关闭confirm 询问框
+            layer.close(index);
+          });
+    })
+})
+
+
+
+
+//获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        //headers就是请求头配置对象
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败!')
+            }
+            //调用函数渲染用户头像
+            renderAvater(res.data)
+        },
+        //不论成功还是失败,都会调用 complete 回调函数
+    })
+}
+
+//渲染用户头像
+function renderAvater(user) {
+    //1.获取用户名称
+    var name = user.nickname || user.username
+    //2.设置欢迎文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    //3.按需渲染用户头像
+    if (user.user_pic !== null) {
+        //3.1渲染图片头像
+        $('.lay-nav-img').attr('src',user.user_pic).show()
+        $('.text-avater').hide()
+    } else {
+        //3.2渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avater').html(first).show()
+    }
+}
+
+//
